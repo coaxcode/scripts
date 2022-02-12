244d1432aa976168ec526762fe07ee17b1cdc65b
Martin Sinkovec <martin.sinkovec@gmail.com>
2022-02-12 14:12:37 +0100

    Updater script for Tumbleweed

---
+++ zupsnap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# zupsnap.sh will upgrade openSUSE Tumbleweed and modify description of created
+# snapshots (pre,post) to release version id's.
+
+
+if  [ $EUID -ne 0 ]
+then
+    echo "Please run as root"
+    exit
+fi
+
+UNIX_DATE=$(date +%s)
+SNAPPER_LIST_FILE="/tmp/zupsnap-${UNIX_DATE}"
+SNAPPER_CURRENT_VERSION_ID=$(cat /etc/os-release | grep VERSION_ID | cut -d'"' -f 2)
+
+echo "Running 'snapper list'"
+snapper --jsonout list > $SNAPPER_LIST_FILE
+
+SNAPPER_LAST_ID=$(jq '.root[-1].number' $SNAPPER_LIST_FILE)
+echo -e "Done!\nLast snapshot ID before upgrade was ${SNAPPER_LAST_ID}"
+
+echo "Running upgrade.."
+zypper ref && zypper dup
+
+echo "Modifying snapshot description"
+SNAPPER_PRE_ID=$((SNAPPER_LAST_ID + 1))
+SNAPPER_POST_ID=$((SNAPPER_LAST_ID + 2))
+
+snapper modify -d "${SNAPPER_CURRENT_VERSION_ID}" $SNAPPER_PRE_ID
+SNAPPER_CURRENT_VERSION_ID=$(cat /etc/os-release | grep VERSION_ID | cut -d'"' -f 2)
+snapper modify -d "${SNAPPER_CURRENT_VERSION_ID}" $SNAPPER_POST_ID
+
+echo "All done!"
